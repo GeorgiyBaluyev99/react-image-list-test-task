9d3f2e085954638e4a1e7be7abb828dac4ad46b4
Georgiy Baluyev <georgiy.baluyev@globallogic.com>
2022-12-14 11:02:55 +0200

    create project skeleton

---
+++ src/components/MainPage/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MainPage = () => {
+  return (
+    <h1>
+        ffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsddfsdfsfd
+    </h1>
+  );
+}
+
+export default MainPage;
